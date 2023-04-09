3a7989805a9698dcfd74b84dabd6c80803430c86
PaulaRamirezGilliland <93882352+PaulaRamirezGilliland@users.noreply.github.com>
2023-04-09 15:47:36 +0100

    "Implemented checkpointing and lr scheduler definition in main"

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ config = {
     # Experiment parameters
     'eval_num': 3,
     'max_iterations': 10000,
+    'batch_size': 12,
 
     # Classifier parameters
     'dropout_class': 0.2,
@@ -54,6 +55,8 @@ def train(config):
     if not os.path.isdir(config['res_dir']):
         os.makedirs(config['res_dir'])
 
+    max_epochs = config['max_iterations'] / config['batch_size']
+
     # Define networks & optimizers for classification and load any pre-trained weights
     if config['experiment_type'] == "classify" or "joint":
         in_channels_class = train_utils.get_in_channels_class(config)
@@ -67,12 +70,18 @@ def train(config):
                                       lr=config['lr_class'],
                                       weight_decay=config['weight_decay_class'])
 
+        lr_scheduler_class = torch.optim.lr_scheduler.LambdaLR(optimizer=optimizer_class,
+                                                               lr_lambda=train_utils.LambdaLR(
+                                                                   max_epochs, 0, 1).step)
+
         # Check if model has been pretrained, and load weights and losses
         losses_train_init_class, losses_valid_init_class, \
-        iteration, epoch = train_utils.try_load_ckpt(config['ckpt_dir'],
-                                                     config['ckpt_name_class'],
-                                                     classifier,
-                                                     optimizer_class)
+        best_metric_class, loss_val_best_class,\
+        iteration_class, epoch_class = train_utils.try_load_ckpt(config['ckpt_dir'],
+                                                                 config['ckpt_name_class'],
+                                                                 classifier,
+                                                                 optimizer_class,
+                                                                 lr_scheduler_class=lr_scheduler_class)
 
     if config['experiment_type'] == "segment" or "joint":
         segmenter = train_utils.cuda(AttentionUnet(spatial_dims=config['spatial_dims'],
@@ -88,73 +97,19 @@ def train(config):
                                       lr=config['lr_seg'],
                                       weight_decay=config['weight_decay_seg'])
 
+        lr_scheduler_seg = torch.optim.lr_scheduler.LambdaLR(optimizer=optimizer_seg,
+                                                               lr_lambda=train_utils.LambdaLR(
+                                                                   max_epochs, 0, 1).step)
+
         # Check if model has been pretrained, and load weights and losses
         losses_train_init_seg, losses_valid_init_seg, \
-        iteration, epoch = train_utils.try_load_ckpt(config['ckpt_dir'],
-                                                     config['ckpt_name_seg'],
-                                                     segmenter,
-                                                     optimizer_seg)
+        best_metric_seg, loss_val_best_seg, \
+        iteration_seg, epoch_seg = train_utils.try_load_ckpt(config['ckpt_dir'],
+                                                             config['ckpt_name_seg'],
+                                                             segmenter,
+                                                             optimizer_seg,
+                                                             lr_scheduler=lr_scheduler_seg)
 
-def load_experiment_from_config(config, classifier=None, segmenter=None,
-                                optimizer_class=None, optimizer_seg=None):
-    """ Loads experiment and losses depending on experiment type (defined in config)
-        Args:
-            config: dictionary containing experiment information (dict)
-            classifier: classifier network to load (pytorch model)
-            segmenter: segmenter network to load (pytorch model)
-            optimizer_class: classifier optimizer (pytorch optimizer)
-            optimizer_seg: segmenter optimizer (pytorch optimizer)
-    """
-    # HERE HAVE TO GET NETWORKS
-    if classifier:
-        ckpt_class = load_checkpoint(ckpt_path='{}latest_classifier.ckpt'.format(
-            config['ckpt_dir']))
-        classifier.load_state_dict(ckpt_class['model'])
-        optimizer_class.load_state_dict(ckpt_class['optimizer'])
-        losses_train_init_class = ckpt_class['losses_train']
-        losses_valid_init_class = ckpt_class['losses_valid']
-        iteration = ckpt_class['iteration']
-        epoch = ckpt_class['epoch']
-    if segmenter:
-        ckpt_seg = load_checkpoint(ckpt_path='{}latest_segmenter.ckpt'.format(
-            config['ckpt_dir']))
-        segmenter.load_state_dict(ckpt_seg['model'])
-
-
-
-    if args.scheduler_class == True:
-        lr_scheduler_class.load_state_dict(ckpt['lr_scheduler'])
-        print("LR SCHEDULER CLASS", optimizer_class.param_groups[0]['lr'])
-
-
-best_valid_loss = best_valid_loss,
-best_metric_valid = best_metrics_valid
-        accuracy_val_best = 1  # ckpt['best_metric']
-        loss_val_best = 10000  # ckpt['best_loss']
-
-        # Loading latest model
-        ckpt = utils.load_checkpoint(
-            '%s/latest_classifier.ckpt' % (os.path.join(ckpt_dir)))
-
-        classifier.load_state_dict(ckpt['model'])
-        optimizer_class.load_state_dict(ckpt['optimizer'])
-        losses_train_init_class = ckpt['losses_train']
-        losses_valid_init_class = ckpt['losses_valid']
-        iteration = ckpt['iteration']
-        epoch = ckpt['epoch']
-
-        if args.scheduler_class == True:
-            lr_scheduler_class.load_state_dict(ckpt['lr_scheduler'])
-            print("LR SCHEDULER CLASS", optimizer_class.param_groups[0]['lr'])
-
-        print("loading pre-trained classifier model from iteration {} and epoch {}".format(iteration, epoch))
-
-    except:
-        print("starting training classifier from scratch")
-        losses_train_init_class = []
-        losses_valid_init_class = []
-        accuracy_val_best = 1e-6
-        loss_val_best = 1e6
 
     # Set up Trainer class
     trainer = train_utils.Trainer(
@@ -172,11 +127,12 @@ best_metric_valid = best_metrics_valid
                                     eval_num=config['eval_num']
                                 )
 
+    """
     if config['training']:
         while iteration < config['max_iterations']:
 
 
-            """
+            
             iteration, losses_train_init, losses_valid_init, dice_val_best, loss_val_best, lr_scheduler = train_ae(
                 model,
                 loss_function,
@@ -200,14 +156,15 @@ best_metric_valid = best_metrics_valid
                 input_type_class=args.input_type_class_AE,
                 use_condition=args.use_condition
             )
-            """
+            
 
                 epoch += 1
+    """
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('hi')
     # Read in args
 
     # Check what experiment
