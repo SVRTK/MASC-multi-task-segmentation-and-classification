bdd9cba373e5dbfaae3fe019cbb1af659b31cea0
PaulaRamirezGilliland <93882352+PaulaRamirezGilliland@users.noreply.github.com>
2023-04-11 18:21:07 +0100

    "Implementing testing for classifier"

--- train/eval.py
+++ train/eval.py
@@ -3,11 +3,18 @@ import numpy as np
 from monai import transforms
 from monai.metrics import compute_meandice, compute_hausdorff_distance, compute_average_surface_distance, \
     get_confusion_matrix, compute_confusion_matrix_metric
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, multilabel_confusion_matrix, \
+    classification_report, balanced_accuracy_score
 import train_utils as utils
 from train_utils import Trainer, cuda
 import os
 import csv
 import nibabel as nib
+import matplotlib.pyplot as plt
+
+# TODO: test class
+# TODO: infer seg
+# TODO: infer class
 
 
 # Testing loop
@@ -18,7 +25,7 @@ class RunTest(Trainer):
         Inherits from utils Trainer class
     """
 
-    def test(self, model, test_files, test_ds):
+    def test_segmenter(self, model, test_files, test_ds):
 
         model.eval()
         post_label = transforms.AsDiscrete(to_onehot=self.N_seg_labels)
@@ -87,7 +94,7 @@ class RunTest(Trainer):
                 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
 
                 pred_names = ["Dice ROI", "HD95 ROI", "avg dist. ROI", "sensitivity ROI", "specificity ROI",
-                          "precision ROI", "casenum"]
+                              "precision ROI", "casenum"]
 
                 # >> Multi-class labels
                 val_labels_multi = post_label(val_labels).unsqueeze(0)
@@ -109,8 +116,7 @@ class RunTest(Trainer):
                 metrics_all.extend([i for i in hausdorff_multi])
                 metrics_all.extend([i for i in avg_surface_dist_multi])
 
-                pred_names = ["Dice ROI", "HD95 ROI", "avg dist. ROI", "sensitivity ROI", "specificity ROI",
-                              "precision ROI"]
+                pred_names = ["Dice ROI", "HD95 ROI", "avg dist. ROI", "sensitivity ROI", "specificity ROI", "precision ROI"]
                 pred_names.extend(["Dice label " + str(i + 1) for i in range(self.N_seg_labels - 1)])
                 pred_names.extend(["HD95 label " + str(i + 1) for i in range(self.N_seg_labels - 1)])
                 pred_names.extend(["ASD label " + str(i + 1) for i in range(self.N_seg_labels - 1)])
@@ -144,4 +150,84 @@ class RunTest(Trainer):
             writer.writerow(avg_metrics)
             writer.writerow(std_names)
             writer.writerow(std_metrics)
+
+    def test_classifier(self, model, test_files, test_ds, segmenter=None):
+
+        model.eval()
+        y_true, y_preds, y_preds_softmax = [], [], []
+
+        for x in range(len(test_files)):
+            case_num = x
+            label = test_ds[case_num]["label"]
+            val_labels = cuda(label, device_num=self.gpu_device)
+            img = test_ds[case_num]["image"]
+            img = cuda(torch.unsqueeze(img, 1), device_num=self.gpu_device)
+            y_true.append(val_labels.item())
+
+            class_in = self.get_input_classifier(img=img, segmenter=segmenter)
+
+            with torch.no_grad():
+                logits = model(class_in)
+
+            # Softmax preds
+            outputs_softmax = torch.squeeze(logits.softmax(dim=1), 0)
+            y_preds_softmax.append(outputs_softmax.detach().cpu().numpy())
+
+            # Argmax outputs for metrics
+            val_outputs = logits.argmax(dim=1)
+            y_preds.append(val_outputs.item())
+
+        y_preds_softmax = np.array(y_preds_softmax)
+
+        # Balanced accuracy
+        balanced_acc = balanced_accuracy_score(y_true, y_preds)
+        labels = ["CoA", "RAA", "DAA"]
+
+        # Generate classification metrics report and save
+        all_metrics_report = classification_report(y_true, y_preds, target_names=labels, sample_weight=None,
+                                                   output_dict=True)
+        print(all_metrics_report)
+        # open file for writing, "w" is writing
+        w = csv.writer(open(self.res_dir + "classification_report.csv", "w"))
+
+        # Convert labelled numbers to conditions
+        y_preds_conds = list(map(utils.convert_num_to_cond, y_preds))
+        y_true_conds = list(map(utils.convert_num_to_cond, y_true))
+        print("Y PREDS = {}, Y PREDS CONDS = {}".format(y_preds, y_preds_conds))
+        print("Y TRUE = {}, Y TRUE CONDS = {}".format(y_true, y_true_conds))
+
+        # loop over dictionary keys and values
+        for key, val in all_metrics_report.items():
+            # write every key and value to file
+            w.writerow([key, val])
+        w.writerow(["Balanced accuracy", balanced_acc])
+        w.writerow(["True labels", y_true])
+        w.writerow(["Pred labels", y_preds])
+        w.writerow(["softmax", y_preds_softmax])
+
+        # Plotting the confusion matrix and saving plot
+        cm = confusion_matrix(y_true_conds, y_preds_conds, labels=labels)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+        plt.figure()
+        disp.plot()
+        plt.savefig(self.res_dir + '/confusion_matrix.png', dpi=200, bbox_inches='tight', transparent=True)
+        plt.savefig(self.res_dir + '/confusion_matrix.eps', format='eps')
+        plt.show()
+
+        # Multi-label confusion matrix (compute CM for each class or sample, one-vs-rest)
+        cm_multi = multilabel_confusion_matrix(y_true_conds, y_preds_conds, labels=labels)
+
+        for ind, cm_ind in enumerate(cm_multi):
+            # Plotting the confusion matrix and saving plot
+            disp = ConfusionMatrixDisplay(confusion_matrix=cm_ind, display_labels=["all", labels[ind]])
+            plt.figure()
+            disp.plot()
+            plt.savefig(self.res_dir + '/' + str(labels[ind]) + '_confusion_matrix.png', dpi=200, bbox_inches='tight',
+                        transparent=True)
+            plt.savefig(self.res_dir + '/' + str(labels[ind]) + '_confusion_matrix.eps', format='eps')
+            plt.show()
+
+        return 0
+
+
 
